e5259e7adba754782625064584e3f4f3c753bb2e
Peter Wooley <peterwooley@gmail.com>
2013-06-25 13:00:27 -0700

    Added PTO command to Arduino.

--- server/app.js
+++ server/app.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res){
-  res.send('<button type="button">PTO!</button><script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script><script>$("button").click(function(){alert("clicked!"); $.post("/pto", {stuff: { "foo" : "bar"} }, function(){}, "json");});</script>');
-});
-
-app.post('/pto', function(req,res){
-	console.log(req);
-    console.log(res);
-});
-
-app.listen(3000);
-console.log('Listening on port 3000');
-
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
-var serialPort = new SerialPort("/dev/tty.usbmodem1411", {
+var serialPort = new SerialPort("/dev/tty.usbmodemfd111", {
   baudrate: 9600,
   parser: serialport.parsers.readline("\n")
 });
@@ -26,6 +13,18 @@ serialPort.open(function () {
     console.log('From Arduino: ' + data);
   });  
 });
+
+app.get('/', function(req, res){
+  res.send('<button type="button">PTO!</button><script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script><script>$("button").click(function(){alert("clicked!"); $.post("/pto", {stuff: { "foo" : "bar"} }, function(){}, "json");});</script>');
+});
+
+app.post('/pto', function(req,res){
+  serialPort.write("pto", function(err, results) { console.log(arguments)})
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
 
 var cmd = 2;
 setInterval(function() {
