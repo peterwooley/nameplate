027cd15e6bd43c017935730ace4a9abc26d1e6be
Jeffrey Hogan <jhogan@jhogan-macair.local>
2013-06-25 12:54:22 -0700

    added express framework and basic inline html/script to create a button and send a request back to Node.js via ajax. Ugly but functional!

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('<button type="button">PTO!</button><script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script><script>$("button").click(function(){alert("clicked!"); $.post("/pto", {stuff: { "foo" : "bar"} }, function(){}, "json");});</script>');
+});
+
+app.post('/pto', function(req,res){
+	console.log(req);
+    console.log(res);
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
-var serialPort = new SerialPort("/dev/tty.usbmodemfd111", {
+var serialPort = new SerialPort("/dev/tty.usbmodem1411", {
   baudrate: 9600,
   parser: serialport.parsers.readline("\n")
 });
